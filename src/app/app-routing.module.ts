01c66172999c2d2f956bc7cdf85010ad50e809c0
unknown <eyjfsaw06@iiht.tech>
2022-11-01 13:17:43 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardViewComponent } from './card-view/card-view.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { CustomerOrdersComponent } from './customer-orders/customer-orders.component';
+import { CustomersComponent } from './customers/customers.component';
+import { EditCustomerComponent } from './edit-customer/edit-customer.component';
+import { ListViewComponent } from './list-view/list-view.component';
+import { MapViewComponent } from './map-view/map-view.component';
+import { OrdersComponent } from './orders/orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"Customers",component:CustomersComponent},
+  {path:"Orders",component:OrdersComponent},
+  {path:"CardView",component:CardViewComponent},
+  {path:"ListView",component:ListViewComponent},
+  {path:"MapView",component:MapViewComponent},
+  {path:"CustomerDetails",component:CustomerDetailsComponent},
+  {path:"CustomerOrders",component:CustomerOrdersComponent},
+  {path:"EditCustomer",component:EditCustomerComponent},
+  {path:"back",component:MapViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { OrdersComponent } from './orders/orders.component';
+import { CardViewComponent } from './card-view/card-view.component';
+import { ListViewComponent } from './list-view/list-view.component';
+import { MapViewComponent } from './map-view/map-view.component';
+import { NewCustomerComponent } from './new-customer/new-customer.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { CustomerOrdersComponent } from './customer-orders/customer-orders.component';
+import { EditCustomerComponent } from './edit-customer/edit-customer.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CustomerService } from './customer.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    OrdersComponent,
+    CardViewComponent,
+    ListViewComponent,
+    MapViewComponent,
+    NewCustomerComponent,
+    CustomerDetailsComponent,
+    CustomerOrdersComponent,
+    EditCustomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CustomerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
